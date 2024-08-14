d79d03f95400bb31a0c9c7e09e6d08708f1a1677
kuddlezpi <chayanne.ssalazar@senacsp.edu.br>
2024-08-14 11:07:48 -0300

    Atualização e modificação do DAOPostagem e metodos

--- src/main/java/br/com/kuddlez/dao/DaoPostagem.java
+++ src/main/java/br/com/kuddlez/dao/DaoPostagem.java
@@ -1,6 +1,7 @@
 package br.com.kuddlez.dao;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 import br.com.kuddlez.dominio.Postagem;
@@ -12,7 +13,7 @@ public class DaoPostagem extends CONEXAO implements CRUDKuddlez<Postagem>{
 		  String msg = "";
 	        try{
 	            if(abrirConexao()){
-	                    String sql = "insert into postagem(dataHoraPost,txtPost,imgPost,videoPost,qtdLikePost,qtdSalvosPost,qtdComentPost,ifComentarioPost)values(?,?,?,?,?,?,?,?)"
+	                    String sql = "insert into postagem(idUsuario,dataHoraPost,txtPost,imgPost,videoPost,qtdLikePost,qtdSalvosPost,qtdComentPost,ifComentarioPost)values(?,?,?,?,?,?,?,?)";
 	                    pst = con.prepareStatement(sql);
 	                   
 	                    pst.setInt(1, dados.getIdUsuario());
@@ -54,25 +55,163 @@ public class DaoPostagem extends CONEXAO implements CRUDKuddlez<Postagem>{
 	
 	@Override
 	public List<Postagem> listar() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+		  List<Postagem> lista = new ArrayList<Postagem>();
+		    try {
+		        if (abrirConexao()) {
+		            String sql = "Select * from postagem order by idPostagem desc";
+		            pst = con.prepareStatement(sql);
+		            rs = pst.executeQuery();
+		            while (rs.next()) {
+		                Postagem post = new Postagem();
+		                post.setIdUsuario(rs.getInt(1));
+		                post.setIdPostagem(rs.getInt(2));
+		                post.setDataHoraPost(rs.getDate(3));
+		                post.setTxtPost(rs.getString(4));
+		                post.setImgPost(rs.getBytes(5));;
+		                post.setVideoPost(rs.getString(6));
+		                post.setQtdLikePost(rs.getString(7));
+		                post.setQtdSalvosPost(rs.getString(8));
+		                post.setQtdComentsPost(rs.getString(9));
+		                post.setIfComentarioPost(rs.getBoolean(10));
+		               
+		                lista.add(post);
+		 
+		            }
+		        }
+		        
+		        else{
+		            new Exception ("Não foi possível estabelecer conexão com o banco");
+		 
+		        }
+		    }
+		    catch(SQLException se) {
+		        new Exception("Erro inesperado"+se.getMessage());
+		    }
+		    catch(Exception e) {
+		        new Exception("Erro inesperado"+e.getMessage());
+		    }
+		    finally{
+		        fecharConexao();
+		    }
+		   return lista;
+		}
+	
 	
 	@Override
 	public Postagem pesquisar(Postagem dados) {
-		// TODO Auto-generated method stub
-		return null;
+		Postagem post = null;
+	    try {
+	        if(abrirConexao()){
+	            String sql = "Select * from postagem where idPostagem=? or txtPost=?";
+	            pst = con.prepareStatement(sql);
+	 
+	            pst.setInt(1, dados.getIdPostagem());
+	            pst.setString(2, dados.getTxtPost());
+	             
+	            rs = pst.executeQuery();
+	 
+	            if(rs.next()) {
+	                post = new Postagem();
+	                post.setIdPostagem(rs.getInt(1));
+	                post.setIdUsuario(rs.getInt(2));
+	                post.setDataHoraPost(rs.getDate(3));
+	                post.setTxtPost(rs.getString(4));
+	                post.setImgPost(rs.getBytes(5));
+	                post.setVideoPost(rs.getString(6));
+	                post.setQtdLikePost(rs.getString(7));
+	                post.setQtdSalvosPost(rs.getString(7));
+	                post.setQtdComentsPost(rs.getString(8));
+	                post.setIfComentarioPost(rs.getBoolean(10));
+	            }
+	        }
+	        else {
+	            new Exception ("Não foi possível estabelecer a conexão com o banco");
+	        }
+	    }
+	 
+	    catch(SQLException se) {
+	        new Exception("Erro inesperado. "+se.getMessage());
+	    }
+	    catch(Exception e) {
+	        new Exception("Erro inesperado. "+e.getMessage());
+	    }
+	    finally {
+	        fecharConexao();
+	    }
+	    return post;
 	}
 	
 	@Override
 	public String atualizar(Postagem dados) {
-		// TODO Auto-generated method stub
-		return null;
+		String msg = "";
+	    try{
+	        if(abrirConexao()) {
+	            String sql = "update postagem set txtPost=? where idPostagem=?";
+	            
+	            pst = con.prepareStatement(sql);
+	            
+	            pst.setString(1,dados.getTxtPost());
+	            
+	            if(pst.executeUpdate() > 0) {
+	            	msg = "Edição realizada";
+	            }
+	            else {
+	            	msg = "Não foi possível editar";
+	            }
+	        }
+	        else {
+				msg= "Não foi possível estabelecer conexão com o banco";
+			 }
+	    }
+	    catch(SQLException se){
+			msg = "Erro ao tentar atualizar"+se.getMessage();
+	    }
+		catch(Exception e) {
+			msg = "Erro inesperado"+e.getMessage();
+		
 	}
+		finally {
+			fecharConexao();
+	}
+	return msg;
+
+}
 	
 	@Override
 	public String apagar(Integer id) {
-		// TODO Auto-generated method stub
-		return null;
+		String msg = "";
+		try {
+			if(abrirConexao()) {
+				String sql = "delete from postagem where idPostagem=?";
+				
+				pst= con.prepareStatement(sql);
+		
+				pst.setInt(1,id);
+				
+				
+				if(pst.executeUpdate() > 0) {
+					msg = "Apagado com sucesso!";
+				}
+				else {
+					msg = "Não foi possível apagar";
+				}
+				
+			}
+			else {
+				msg= "Não foi possível estabelecer conexão com o banco";
+			}
+			
+			}
+			catch(SQLException se){
+				msg = "Erro ao tentar apagar"+se.getMessage();
+		}
+			catch(Exception e) {
+				msg = "Erro inesperado"+e.getMessage();
+			
+		}
+			finally {
+				fecharConexao();
+		}
+		return msg;
 	}
 }
