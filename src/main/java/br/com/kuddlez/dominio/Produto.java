0eabd22058ba5eee8601845bce1bc06399623bfc
kuddlezpi <chayanne.ssalazar@senacsp.edu.br>
2024-08-14 11:33:20 -0300

    Criação do dominio Produto

---
+++ src/main/java/br/com/kuddlez/dominio/Produto.java
@@ -0,0 +1,71 @@
+package br.com.kuddlez.dominio;
+
+import java.sql.Date;
+
+public class Produto {
+	private Integer idProduto;
+	private Integer idUsuario;
+	private String nomeProd;
+	private Double precoProd;
+	private String descProd;
+	private Integer qtdProd;
+	private Date dataCadastroProd;
+	private String categoriaProd;
+	private Boolean possiTrocaProd;
+	
+	public Integer getIdProduto() {
+		return idProduto;
+	}
+	public void setIdProduto(Integer idProduto) {
+		this.idProduto = idProduto;
+	}
+	public Integer getIdUsuario() {
+		return idUsuario;
+	}
+	public void setIdUsuario(Integer idUsuario) {
+		this.idUsuario = idUsuario;
+	}
+	public String getNomeProd() {
+		return nomeProd;
+	}
+	public void setNomeProd(String nomeProd) {
+		this.nomeProd = nomeProd;
+	}
+	public Double getPrecoProd() {
+		return precoProd;
+	}
+	public void setPrecoProd(Double precoProd) {
+		this.precoProd = precoProd;
+	}
+	public String getDescProd() {
+		return descProd;
+	}
+	public void setDescProd(String descProd) {
+		this.descProd = descProd;
+	}
+	public Integer getQtdProd() {
+		return qtdProd;
+	}
+	public void setQtdProd(Integer qtdProd) {
+		this.qtdProd = qtdProd;
+	}
+	public Date getDataCadastroProd() {
+		return dataCadastroProd;
+	}
+	public void setDataCadastroProd(Date dataCadastroProd) {
+		this.dataCadastroProd = dataCadastroProd;
+	}
+	public String getCategoriaProd() {
+		return categoriaProd;
+	}
+	public void setCategoriaProd(String categoriaProd) {
+		this.categoriaProd = categoriaProd;
+	}
+	public Boolean getPossiTrocaProd() {
+		return possiTrocaProd;
+	}
+	public void setPossiTrocaProd(Boolean possiTrocaProd) {
+		this.possiTrocaProd = possiTrocaProd;
+	}
+
+}
