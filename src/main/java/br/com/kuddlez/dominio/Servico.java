82269176a6ac1ea1254488fc71f58433c63842f6
camila jasmin <camilajasmin.ec@gmail.com>
2024-08-14 17:09:23 -0300

    atualizando daoServico e encapsulando

--- src/main/java/br/com/kuddlez/dominio/Servico.java
+++ src/main/java/br/com/kuddlez/dominio/Servico.java
@@ -1,5 +1,63 @@
 package br.com.kuddlez.dominio;
 
 public class Servico {
+	private Integer idServico;
+	private Integer idUsuario;
+	private String funcoesServ;
+	private String descServ;
+	private String contatoServ;
+	private String dispoServ;
+	private String valorServ;
+	private Integer qtdRealizadosServ;
+	
+	public Integer getIdServico() {
+		return idServico;
+	}
+	public void setIdServico(Integer idServico) {
+		this.idServico = idServico;
+	}
+	public Integer getIdUsuario() {
+		return idUsuario;
+	}
+	public void setIdUsuario(Integer idUsuario) {
+		this.idUsuario = idUsuario;
+	}
+	public String getFuncoesServ() {
+		return funcoesServ;
+	}
+	public void setFuncoesServ(String funcoesServ) {
+		this.funcoesServ = funcoesServ;
+	}
+	public String getDescServ() {
+		return descServ;
+	}
+	public void setDescServ(String descServ) {
+		this.descServ = descServ;
+	}
+	public String getContatoServ() {
+		return contatoServ;
+	}
+	public void setContatoServ(String contatoServ) {
+		this.contatoServ = contatoServ;
+	}
+	public String getDispoServ() {
+		return dispoServ;
+	}
+	public void setDispoServ(String dispoServ) {
+		this.dispoServ = dispoServ;
+	}
+	public String getValorServ() {
+		return valorServ;
+	}
+	public void setValorServ(String valorServ) {
+		this.valorServ = valorServ;
+	}
+	public Integer getQtdRealizadosServ() {
+		return qtdRealizadosServ;
+	}
+	public void setQtdRealizadosServ(Integer qtdRealizadosServ) {
+		this.qtdRealizadosServ = qtdRealizadosServ;
+	}
+	
 
 }
