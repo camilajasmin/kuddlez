8380d8bf82f5d19f4c800f176585559116caf75f
camila jasmin <camilajasmin.ec@gmail.com>
2024-08-14 20:49:53 -0300

    Criação do Dao de Serviço e adição de métodos

---
+++ src/main/java/br/com/kuddlez/dao/DaoServico.java
@@ -0,0 +1,52 @@
+package br.com.kuddlez.dao;
+
+import java.util.List;
+
+import br.com.kuddlez.dominio.Servico;
+
+public class DaoServico extends CONEXAO implements CRUDKuddlez<Servico> {
+
+	@Override
+	public String Cadastrar(Servico dados) {
+		String msg = "";
+		try {
+			if (abrirConexao()) {
+				String sql = "insert into servico";
+				
+			} 
+			else {
+
+			}
+			
+		} 
+		finally {
+			
+		}
+		return msg;
+	}
+
+	@Override
+	public List<Servico> listar() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Servico pesquisar(Servico dados) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String atualizar(Servico dados) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String apagar(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
