d0470375a731c92ff0610d91d7b04993e9c97902
kuddlezpi <chayanne.ssalazar@senacsp.edu.br>
2024-08-20 11:56:44 -0300

    Criação testVenda

---
+++ src/main/java/br/com/kuddlez/test/testVenda.java
@@ -0,0 +1,28 @@
+package br.com.kuddlez.test;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import br.com.kuddlez.dao.DaoVenda;
+import br.com.kuddlez.dominio.Venda;
+
+public class testVenda {
+	@Test
+	public void testCadastrar(){
+		Venda ven = new Venda();
+		
+		ven.setIdProduto(2);
+		ven.setVendedorVenda(4);
+		ven.setCompradorVenda(2);
+		ven.setDataHoraVenda(new java.sql.Date(System.currentTimeMillis()));
+		ven.setFormaPagamentoVenda("Dinheiro");
+		ven.setValorTotalVenda(100.0);
+		ven.setStatusVenda("Comprar");
+		
+		DaoVenda daoven = new DaoVenda();
+		assertEquals("Cadastro realizado",daoven.Cadastrar(ven));
+		
+	}
+
+}
